9b8e6e76eb23066bc55be1524cdaf3406a4fedb1
srinivas-tanla <srinivas.mannem@tanla.com>
2024-03-11 18:15:17 +0530

    updated port

--- dags/mysql_consent_dag.py
+++ dags/mysql_consent_dag.py
@@ -11,7 +11,7 @@ import os
 
 os.environ["no_proxy"] = "*"
 
-URL = 'http://138.201.63.104:5000/submit'
+URL = 'http://138.201.63.104:5010/submit'
 
 # MySQL Server Connection Details
 # host = 'your_mysql_host'
